e28a9aa457edfcc3f5b85d4c3c95aadb015c37cc
cRanDy-cRinGe <137729605+cRanDy-cRinGe@users.noreply.github.com>
2024-10-04 21:23:50 +0300

    Update tonconnect.js

--- tonconnect.js
+++ tonconnect.js
@@ -1,24 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // Лог для перевірки доступності TonConnect
     console.log('Перевірка доступності TonConnect:', typeof TonConnect); // Лог для перевірки доступності
 
     // Перевірка доступності TonConnect
     if (typeof TonConnect === 'undefined') {
         console.error('TonConnect SDK не завантажено');
-        return;
+        return; // Вихід, якщо SDK не завантажено
     }
 
-    console.log('TonConnect SDK завантажено:', TonConnect);
-
     // Ініціалізація TonConnect
-    const tonConnect = new TonConnect({
-        manifestUrl: 'https://crandy-cringe.github.io/land_1.0/tonconnect-manifest.json'
+    const tonConnect = new TonConnect({ 
+        manifestUrl: 'https://cdn.jsdelivr.net/npm/@tonconnect/sdk@latest/dist/tonconnect-sdk.min.js' 
     });
 
     // Обробник події для кнопки підключення
     document.getElementById('btn').addEventListener('click', async () => {
         try {
             const wallets = await tonConnect.getWallets();
-            console.log('Доступні гаманці:', wallets);
             const tonkeeper = wallets.find(wallet => wallet.name === 'Tonkeeper');
 
             if (!tonkeeper) {
