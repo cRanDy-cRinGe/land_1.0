0316363fe8798027be871097507c64343e56198e
cRanDy-cRinGe <137729605+cRanDy-cRinGe@users.noreply.github.com>
2024-10-04 19:37:02 +0300

    Update tonconnect.js

--- java/tonconnect.js
+++ java/tonconnect.js
@@ -1,26 +1,19 @@
-// Підключаємо TonConnect через CDN
 const tonConnect = new TonConnect({ manifestUrl: 'https://crandy-cringe.github.io/land_1.0/manifest.json' });
 
-// Встановлення обробника на кнопку для підключення
 document.getElementById('btn').addEventListener('click', async () => {
     try {
-        // Отримуємо список підтримуваних гаманців
         const wallets = await tonConnect.getWallets();
-
-        // Фільтруємо список для Tonkeeper
         const tonkeeper = wallets.find(wallet => wallet.name === 'Tonkeeper');
 
         if (!tonkeeper) {
             throw new Error('Tonkeeper не знайдено серед доступних гаманців');
         }
 
-        // Ініціюємо підключення
         await tonConnect.connectWallet({
             universalLink: tonkeeper.universalLink,
             bridgeUrl: tonkeeper.bridgeUrl,
         });
 
-        // Перевірка підключеного гаманця
         if (tonConnect.wallet) {
             const userInfo = tonConnect.wallet.account;
             console.log('Підключено до Tonkeeper:', userInfo);
