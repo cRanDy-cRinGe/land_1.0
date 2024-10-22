34a1cf5900e8834e3e031fbc16bc7ee9fa5478a4
cRanDy-cRinGe <137729605+cRanDy-cRinGe@users.noreply.github.com>
2024-10-22 17:24:50 +0300

    Update tonconnect-wallet.js

--- java/tonconnect-wallet.js
+++ java/tonconnect-wallet.js
@@ -9,8 +9,59 @@ if (walletAddress) {
 } else {
     document.getElementById('walletAddress').innerText = 'Wallet not connected';
 }
+tonConnectUI.onStatusChange(async (wallet) => {
+    if (wallet && wallet.account) {
+        const walletAddress = wallet.account.address;
+        localStorage.setItem('walletAddress', walletAddress);
 
+        // Отримуємо баланс TON з TON API
+        await updateTonBalance(walletAddress);
 
+        // Оновлюємо баланс TCL
+        await getBalance(walletAddress); // Оновлюємо баланс після підключення
+    } else {
+        console.error('Wallet not connected');
+    }
+});
+async function updateTonBalance(walletAddress) {
+    const apiUrl = `https://toncenter.com/api/v2/getAddressBalance?address=${walletAddress}`;
+    try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        const balanceInNano = data.result;
+        const balanceInTon = balanceInNano / Math.pow(10, 9);
+
+        document.getElementById('ton-balance').innerText = `${balanceInTon.toFixed(3)} TON`;
+    } catch (error) {
+        console.error('Error fetching balance:', error);
+        document.getElementById('ton-balance').innerText = 'Error fetching balance';
+    }
+}
+async function getBalance(walletAddress) {
+    try {
+        const response = await fetch('http://localhost:3000/get-balance', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ walletAddress })
+        });
+
+        const data = await response.json();
+        console.log('Balance response:', data); // Лог для перевірки
+
+        if (response.ok) {
+            // Перетворюємо balance в число
+            const balance = parseFloat(data.balance);
+            document.getElementById('tcl-balance').innerText = `${balance.toFixed(4)} TCL`;
+        } else {
+            console.error('Error fetching balance:', data.error);
+        }
+    } catch (error) {
+        console.error('Error fetching balance:', error);
+    }
+}
 // Функція для відправки транзакції
 document.getElementById('send').addEventListener('click', async () => {
     if (walletAddress) {
